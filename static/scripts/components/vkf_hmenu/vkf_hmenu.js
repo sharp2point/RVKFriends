43d1e0ad0c1a2202eced530b343ee8761c9fba19
Blinov Evgeny <sharppoint@yandex.ru>
2022-08-22 16:53:35 +0300

    перенес стили CSS в веб компоненты

--- static/scripts/components/vkf_hmenu/vkf_hmenu.js
+++ static/scripts/components/vkf_hmenu/vkf_hmenu.js
@@ -3,8 +3,8 @@ import ItemMenu from "./vkf_item_menu.js";
 class HMenu extends HTMLElement {
   connectedCallback() {
     let items_tmpl = [];
-    let icons = this.getAttribute("icons").split(' ');
-    let items = this.getAttribute("items").split(' ');
+    let icons = this.getAttribute("icons").split(" ");
+    let items = this.getAttribute("items").split(" ");
     for (let i = 0; i < items.length; i++) {
       if (i === 0) {
         items_tmpl.push(
@@ -17,16 +17,89 @@ class HMenu extends HTMLElement {
       }
     }
     this.innerHTML = `
-        <div class="menu-sort">
-            <ul>
+            <div class="items">
                 ${items_tmpl[0]}
                 ${items_tmpl[1]}
                 ${items_tmpl[2]}
                 ${items_tmpl[3]}
                 ${items_tmpl[4]}                
                 <div class="indicator"></div>
-            </ul>
-        </div>`;
+            </div>
+            `;
+
+    this.innerHTML+=`
+        <style>
+        .menu-sort{
+            position: relative;
+            width: 400px;
+            height: 70px;
+            background: #fff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 10px;
+        }
+        .menu-sort .items{
+            display: flex;
+            flex-direction: row;
+            width: 350px;
+        }
+        .menu-sort .items vkf-item-menu{
+            position: relative;
+            list-style:none;
+            width: 70px;
+            height: 70px;
+            z-index: 1;
+        }
+        .indicator{
+            position: absolute;
+            width: 70px;
+            height: 70px;
+            background: #29fd53;
+            top:-50%;
+            border-radius: 50%;
+            border: 6px solid var(--clr);
+            transition: 0.5s;
+        }
+        .indicator::before{
+            content:'';
+            position: absolute;    
+            top: 50%;
+            left:-22px;
+            width: 20px;
+            height: 20px;
+            background: transparent;
+            border-top-right-radius: 20px;
+            box-shadow: 0px -10px 0 0 var(--clr);
+        }
+        .indicator::after{
+            content:'';
+            position: absolute;
+            top:50%;
+            right: -22px;
+            width: 20px;
+            height: 20px;
+            background: transparent;
+            border-top-left-radius: 20px;
+            box-shadow: 0px -10px 0 0 var(--clr);
+        }
+        .menu-sort .items vkf-item-menu:nth-child(1).active ~ .indicator{
+            transform: translateX(calc(70px*0));
+        }
+        .menu-sort .items vkf-item-menu:nth-child(2).active ~ .indicator{
+            transform: translateX(calc(70px*1));
+        }
+        .menu-sort .items vkf-item-menu:nth-child(3).active ~ .indicator{
+            transform: translateX(calc(70px*2));
+        }
+        .menu-sort .items vkf-item-menu:nth-child(4).active ~ .indicator{
+            transform: translateX(calc(70px*3));
+        }
+        .menu-sort .items vkf-item-menu:nth-child(5).active ~ .indicator{
+            transform: translateX(calc(70px*4));
+        }
+        </style>
+    `;
   }
 }
 if (!customElements.get("vkf-hmenu")) {
--- static/scripts/components/vkf_hmenu/vkf_item_menu.js
+++ static/scripts/components/vkf_hmenu/vkf_item_menu.js
@@ -1,22 +1,63 @@
 class ItemMenu extends HTMLElement {
   connectedCallback() {
-    let name = this.getAttribute("name")
-    let icon = this.getAttribute("icon")
-    let active = this.getAttribute("active")
+    let name = this.getAttribute("name");
+    let icon = this.getAttribute("icon");
+    let active = this.getAttribute("active");
     this.innerHTML = `
-                <a href="#">
-                    <span class="icon"><ion-icon name="${icon}"></ion-icon></span>
-                    <span class="text">${name}</span>
-                </a>
+          <a href="#">
+            <span class="icon"><ion-icon name="${icon}"></ion-icon></span>
+            <span class="text">${name}</span>
+          </a>
         `;
-    this.classList.add("list")
-    if(active==="true"){
-      this.classList.add("active")
-    }    
+    this.classList.add("list");
+    if (active === "true") {
+      this.classList.add("active");
+    }
+
+    this.innerHTML += `
+    <style>
+        a{
+          position: relative;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+          width: 100%;
+          text-align: center;
+          font-weight: 500;
+        }
+        a .icon{
+            position: relative;
+            display: block;
+            line-height: 75px;
+            font-size: 1.5em;
+            text-align: center;
+            transition: 0.5s;
+            color: var(--clr);
+        }
+        .active a .icon{
+            transform: translateY(-35px);
+        }
+        a .text{
+            position: absolute;
+            color: var(--clr);
+            font-weight: 400;
+            font-size: 0.75em;
+            letter-spacing: 0.05;
+            transition: 0.5s;
+            opacity: 0;
+            transform: translateY(20px);
+        }
+        .active a .text{
+            opacity: 1;
+            transform: translateY(10px);
+        }
+      </style>
+    `;
   }
 }
 if (!customElements.get("vkf-item-menu")) {
   customElements.define("vkf-item-menu", ItemMenu);
 }
 
-export default ItemMenu 
+export default ItemMenu;
